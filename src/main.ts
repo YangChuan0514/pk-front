341e992f360550888cde36c66d616b0c60c40539
yangchuan <yangchuan@hortorgames.com>
2026-01-19 11:15:31 +0800

    feat: css

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'reset-css'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,16 @@ import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-  <main>
+  <main class="main">
     <HelloWorld msg="Hello World" />
   </main>
 </template> 
+<style scoped lang="scss">
+.main {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f0f0;
+  color: red;
+}
+</style>
 
