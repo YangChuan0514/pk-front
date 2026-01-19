4404534b2685601ca967ad678d06b08f00ac955f
yangchuan <yangchuan@hortorgames.com>
2026-01-19 15:04:48 +0800

    feat: css 去除 reset-css

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import 'reset-css'
 import '@/assets/main.css'
 import 'uno.css'
 const app = createApp(App)
